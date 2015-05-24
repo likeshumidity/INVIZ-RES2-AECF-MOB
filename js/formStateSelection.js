ba444cb75aa48937537a6760ab3d4cab49dcf57b
Jacob Joseph <jjoseph03@mica.edu>
2015-05-24 12:50:38 -0400

    added viewbox to svg

--- js/formStateSelection.js
+++ js/formStateSelection.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     rows.forEach( function getvalues(rowState) {
       cols = rowState.split(',');
 //      console.log(cols[1] + ' - ' + cols[0]);
-      if (cols[0] != 'Name') {
+      if ((cols[0] != 'Name') && (cols[0].length > 0)) {
         html += '<option value="' + cols[1] + '">' + cols[0] + '</option>';
       }
     });
